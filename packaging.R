f8852c70e7c98b83b8af45abd451860d81b2bbc3
mayer79 <mayermichael79@gmail.com>
2021-11-27 17:51:20 +0100

    improve workflow description

--- packaging.R
+++ packaging.R
@@ -5,34 +5,41 @@
 # Much more info on https://r-pkgs.org/
 #=====================================================================================
 
-# Firstly, clean up some files and folders. They will be generated later.
+# Clean up some files and folders. They will be generated later.
 for (f in c(".Rbuildignore", "DESCRIPTION", "NAMESPACE", "LICENSE.md", "R/utils-pipe.R"))
   if (file.exists(f)) file.remove(f)
 for (f in c("doc", "Meta", "man"))
   if (dir.exists(f)) unlink(f, recursive = TRUE)
 
-# WORKFLOW (without clicking any buttons...)
-# 0) Rename main folder to YOUR package name.
-# 1) If you don't need them, delete folders "tests" and/or "vignettes".
-# 2) Replace word "prettybreaks" in all files and file names by your package name
+# WORKFLOW FOR CREATING A NEW PACKAGE
+# 1) Clone this template from https://github.com/mayer79/prettybreaks.
+# 2) Rename main folder to YOUR package name, remove folder ".git" and
+#    create a fresh git tracking (in RStudio or by running "git init").
+# 3) If you don't need them, delete folders "tests" and/or "vignettes".
+# 4) Replace word "prettybreaks" in all files and file names by your package name
 #    (NEWS.md, packaging,R, *.Rproj, README.md, tests/, vignettes/.
-# 3) Put user relevant functions of your package into folder "R" as [function_name].R each.
+# 5) Put user visible functions of your package into folder "R" as [function_name].R each.
 #    Helper functions can be added (undocumented) to a "utils.R" script
-#    or in any of the existing scripts in R/.
-# 4) Edit NEWS.md, README.md, packaging.R and the content of the folders
-#    tests/ and vignette/ to describe your package.
-#    If the package should go to CRAN, also edit cran-comments.md.
-#    Restart RStudio.
-# 5) Run this script. It will generate a zip and tar.gz package that can be distributed.
-#    The vignette is not part of the package but can be found in doc/
+#    or in any of the existing scripts in folder "R".
+# 6) Edit NEWS.md, README.md, packaging.R and the content of the optional folders
+#    "tests" and "vignette" to describe your package.
+#    If the package should go to CRAN, edit file "cran-comments.md" as well.
+# 7) Restart RStudio and run this script. It will generate a zip and tar.gz package.
+#    The vignette is not part of the package but can be found in folder "doc".
+
+# WORKFLOW TO UPDATE PACKAGE TO NEW VERSION
+# 1) Update functions, documentation (NEWS.md, README.md, vignettes, tests,
+#    and - if package is on CRAN - also cran-comments.md).
+# 2) Increase package version in the script below and run this script.
 
 # NOTES
 # - Should something go wrong, fix the problems and rerun this script.
 # - In your R-scripts, never load a library or run "source".
 # - In your R-scripts, the use of "packagename::function" is fine, while
 #   you can't use internal functions like "packagename:::function".
-# - Keep dependency footprint small.
-# - If the package should go to CRAN, no warnings or notes should be left.
+# - Keep the dependency footprint small, i.e., don't use unnecessary packages.
+# - If the package should go to CRAN, no warnings/notes should appear in check().
+# - Git tracking is recommended, but not strictly required.
 
 library(usethis)
 library(devtools)
